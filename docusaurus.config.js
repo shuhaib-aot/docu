7ab2b631baf6a967cd08b855215876d08283a7c2
shuhaib-aot <shuhaib.s@aot-technologies.com>
2024-05-06 12:57:25 +0530

    fixed depoy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,13 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://suhaib-aot.github.io', // Your website URL
   baseUrl: '/',
+  projectName: 'docu',
+  organizationName: 'Aot',
+  trailingSlash: false,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+ 
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
